679a7209ad18da0b9add1611d98d69e1cc51afa7
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2022-04-07 01:25:31 +0200

    Szablon aplikcji: -stylowanie -przejscie z ekranu logowania na ekaran aplikacji

--- src/main/java/car/serwis/carserwis/controller/LoginController.java
+++ src/main/java/car/serwis/carserwis/controller/LoginController.java
@@ -1,17 +1,24 @@
 package car.serwis.carserwis.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
+
+    private static final String APP_FXML = "/fxml/app.fxml";
+    private static final String APP_TITLE = "Car Serwis";
 
     @FXML
     private Button exitButton;
@@ -31,6 +38,7 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         initializeExitButton();
+        initializeLoginButton();
     }
 
 
@@ -40,8 +48,41 @@ public class LoginController implements Initializable {
         });
     }
 
+    private void initializeLoginButton() {
+        loginButton.setOnAction((x) -> {
+            AuthenticationUser();
+        });
+    }
+
     private Stage getStage(){
         return (Stage) loginAnchorPane.getScene().getWindow();
+    }
+
+    private void openAppAndCloseLoginStage(){
+        Stage appStage = new Stage();
+        Parent appRoot = null;
+
+        try {
+            appRoot = FXMLLoader.load(getClass().getResource(APP_FXML));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(appRoot,1600,900);
+        appStage.setTitle(APP_TITLE);
+        appStage.setScene(scene);
+        appStage.show();
+        getStage().close();
+    }
+
+    private void AuthenticationUser(){
+        String login = loginTextField.getText();
+        String haslo = hasloTextField.getText();
+
+        if (login.equals("admin") && haslo.equals("admin")){
+            openAppAndCloseLoginStage();
+        }else {
+            System.out.println("false");
+        }
     }
 
 }
