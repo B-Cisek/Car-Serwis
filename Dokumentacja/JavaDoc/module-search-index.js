017f4558137ae00f8b038e11530cd2bb323f0d85
Bartłomiej Cisek <bartlomiej.cisek@outlook.com>
2022-06-22 02:08:45 +0200

    Dokumentacja + Instalator

---
+++ Dokumentacja/JavaDoc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"car.serwis.carserwis"}];updateSearchResults();
--- test/car/serwis/helpers/ValidatorFieldsTest.java
+++ test/car/serwis/helpers/ValidatorFieldsTest.java
@@ -2,7 +2,6 @@ package car.serwis.helpers;
 
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ValidatorFieldsTest {
